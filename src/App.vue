feaee5aa69c117f0a145f1175a615ded39f72bb6
Goks <firehawk005@gmail.com>
2019-07-02 16:07:01 +0530

    components Communicating

--- src/App.vue
+++ src/App.vue
@@ -13,23 +13,23 @@
 </template>
 
 <script>
-    import Header from './components/Header.vue';
-    import Footer from './components/Footer.vue';
-    import Servers from './components/Servers.vue';
-    import ServerDetails from './components/ServerDetails.vue';
+    // import Header from './components/Header.vue';
+    // import Footer from './components/Footer.vue';
+    // import Servers from './components/Servers.vue';
+    // import ServerDetails from './components/ServerDetails.vue';
     import User from './components/User.vue';
 
     export default {
         components: {
-            'app-header': Header,
-            'app-servers': Servers,
-            'app-server-details': ServerDetails,
-            'app-footer': Footer,
             'appUser': User
         }
     }
 </script>
 
 <style>
+    div.component {
+        border: 1px solid black;
+        padding: 30px;
+    }
 
 </style>

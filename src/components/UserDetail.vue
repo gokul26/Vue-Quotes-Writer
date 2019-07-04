9757caaa4fc387a47242bb73530024baecc88001
Goks <firehawk005@gmail.com>
2019-07-04 14:56:04 +0530

    component from Child to Parent

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,13 +2,23 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>User name: {{name}}</p>
+        <p>User name: {{changeName()}}</p>
+        <button @click="resetName()">Change Name</button>
     </div>
 </template>
 
 <script>
 export default{
-    props: ['name']
+    props: ['name'],
+    methods:{
+        changeName: function() {
+            return this.name.split("").reverse().join("");
+        },
+        resetName: function(){
+            this.name = 'Max';
+            this.$emit('nameChanged', this.name);
+        }
+    }
 }
 </script>
 
